9dd87d84864182d79fdd34ac4ae09dede6b54e9e
Renthop <alenapopova24al1@gmail.com>
2023-02-19 21:22:58 +0300

    Init project

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/worksans.css">
+  <title>BAGAT</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <header>
+      <a href="index.php" class="header__logo">
+        <img src="img/logo.svg" alt="Logo">
+      </a>
+      <nav>
+        <ul class="header__nav">
+          <li class="header__nav-link">
+            <a href="#">Рассчитать доставку</a>
+          </li>
+          <li class="header__nav-link">
+            <a href="#">Дополнительные услуги</a>
+          </li>
+          <li class="header__nav-link">
+            <a href="#">Контакты</a>
+          </li>
+        </ul>
+        <div class="main__img">
+        <img src="img/headerIMG.svg" alt="header img">
+      </div>
+      </nav>
+    </header>
+    <main>
+      <h1>НОЧНОЙ ЭКСПРЕСС</h1>
+      <p>Мы предлагаем доставку сборных грузов из Москвы в Санкт-Петербург
+          на следующий день.</p>
+      <p>Принимаем грузы ежедневно до <span>21:00</span>, выдача на следующий день с <span>09:00</span></p>
+    </main>
+    <section class="services">
+      <h2>Почему мы</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictum suspendisse sed vel a, egestas ut tellus est, at. Massa pretium sed lacus sed nibh sed nunc praesent. Nulla fermentum, malesuada enim.</p>
+      <div class="service__card-wrapper">
+        <div id="item-0" class="service__card">
+          <img src="img/service/service_1.svg" alt="Service Card">
+          <p>Спутниковое отслеживание груза</p>
+        </div>
+        <div id="item-1" class="service__card">
+        <img src="img/service/service_2.svg" alt="Service Card">
+          <p>Собственные терминалы
+              в Москве и
+              Санкт -Петербурге</p>
+        </div>
+        <div id="item-2" class="service__card">
+        <img src="img/service/service_3.svg" alt="Service Card">
+          <p>Любые грузы</p>
+        </div>
+        <div id="item-3" class="service__card">
+        <img src="img/service/service_4.svg" alt="Service Card">
+          <p>Отдельный автомобиль</p>
+        </div>
+        <div id="item-4" class="service__card">
+        <img src="img/service/service_5.svg" alt="Service Card">
+          <p>Передадим адресату «из рук в руки»</p>
+        </div>
+        <div id="item-5" class="service__card">
+        <img src="img/service/service_6.svg" alt="Service Card">
+          <p>Ежедневные отправки</p>
+        </div>
+        <div id="item-6" class="service__card">
+        <img src="img/service/service_7.svg" alt="Service Card">
+          <p>Заберем и доставим в любую точку города и области</p>
+        </div>
+        <div id="item-7" class="service__card">
+        <img src="img/service/service_8.svg" alt="Service Card">
+          <p>Наличие собственного парка транспортных средств</p>
+        </div>
+      </div>
+    </section>
+    <section class="checktown">
+      <h3>Выберите город, в котором требуется услуга ответственного хранения</h3>
+      <div id="tabs1" class="tabs">
+        <div class="tabRow">
+          <ul>
+            <li class="tab">Москва</li>
+            <li class="tab">Ростов-На-Дону</li>
+            <li class="tab">Тариф на Терминальную обработку грузов и Ответственное хранение</li>
+          </ul>
+        </div>
+        <div class="tabContainer">
+          <div class="tabContent">
+            <div class="tabContent__right">
+              <p>Следует отметить, что синтетическое тестирование играет определяющее значение для распределения внутренних резервов и ресурсов. Значимость этих проблем настолько очевидна, что разбавленное изрядной долей эмпатии, рациональное мышление однозначно фиксирует необходимость форм воздействия. Разнообразный и богатый опыт говорит нам, что сложившаяся структура организации предопределяет высокую востребованность направлений прогрессивного развития. Как уже неоднократно упомянуто, тщательные исследования конкурентов формируют глобальную экономическую сеть и при этом — заблокированы в рамках своих собственных рациональных ограничений. Разнообразный и богатый опыт говорит нам, что существующая теория создаёт предпосылки для дальнейших направлений развития. Прежде всего, высококачественный прототип будущего проекта играет определяющее значение для позиций, занимаемых участниками в отношении поставленных задач.</p>
+            </div>
+          </div>
+          <div class="tabContent">
+            <div class="tabContent__right">
+              <p>Значимость этих проблем настолько очевидна, что социально-экономическое развитие однозначно фиксирует необходимость поставленных обществом задач. Учитывая ключевые сценарии поведения, начало повседневной работы по формированию позиции способствует повышению качества первоочередных требований. Каждый из нас понимает очевидную вещь: постоянный количественный рост и сфера нашей активности позволяет оценить значение новых предложений. Есть над чем задуматься: сделанные на базе интернет-аналитики выводы представлены в исключительно положительном свете. Предварительные выводы неутешительны: экономическая повестка сегодняшнего дня играет определяющее значение для соответствующих условий активизации. Но курс на социально-ориентированный национальный проект играет определяющее значение для прогресса профессионального сообщества.</p>
+            </div>
+          </div>
+          <div class="tabContent">
+            <div class="tabContent__right">
+              <p>Такая услуга, как ответственное хранение позволит временно поместить Ваш груз на нашем складе, оплатив только занимаемую площадь. Таким образом, сократив Ваши расходы на логистические услуги.
+                Вы получите площадь хранения любого нужного размера, с возможностью изменения. Мы возьмём на себя комплектацию, формирование заказов (внутреннюю проверку, маркировку, сортировку, переупаковку и т.п.) и осуществим адресную доставку клиентам.</p>
+            </div>
+          </div>
+        </div>
+     </div>
+    </section>
+    <section>
+      <div class="about__left about__classic">
+        <img src="img/about/img_2.svg" alt="About Img" class="about__left-img">
+        <div class="about__content">
+          <h4>О направлении</h4>
+          <div class="border"></div>
+          <p>Одно из основных направлений автоперевозок нашей компании - это доставка грузов по маршруту из Москвы в Санкт-Петербург, а также из Санкт-Петербурга в Москву.</p>
+          <p>Благодаря наличию собственного автотранспорта и четко выстроенной схеме движения, мы доставим Ваш груз между столицами на следующий день до нашего склада или адреса получателя. Все машины оборудованы инновационными системами навигации, что дает возможность отслеживать груз во время всей перевозки.</p>
+          <p>Доставка грузов по данному маршруту осуществляется ежедневно. Мы выполняем доставку «Москва ⇄ Санкт-Петербург» как в составе сборного груза, так и индивидуальным автотранспортом.</p>
+        </div>
+      </div>
+      <div class="about__right">
+      <img src="img/about/img_1.svg" alt="About Img">
+        <div class="about__content">
+          <h4>Основные требования к упаковке груза</h4>
+          <div class="border"></div>
+          <p>Одно из основных направлений автоперевозок нашей компании - это доставка грузов по маршруту из Москвы в Санкт-Петербург, а также из Санкт-Петербурга в Москву.</p>
+          <p>Благодаря наличию собственного автотранспорта и четко выстроенной схеме движения, мы доставим Ваш груз между столицами на следующий день до нашего склада или адреса получателя. Все машины оборудованы инновационными системами навигации, что дает возможность отслеживать груз во время всей перевозки.</p>
+          <p>Доставка грузов по данному маршруту осуществляется ежедневно. Мы выполняем доставку «Москва ⇄ Санкт-Петербург» как в составе сборного груза, так и индивидуальным автотранспортом.</p>
+        </div>
+      </div>
+      <div class="about__left">
+      <img src="img/about/img_3.svg" alt="About Img">
+        <div class="about__content">
+          <h4>Дополнительные услуги</h4>
+          <div class="border"></div>
+          <p>Одно из основных направлений автоперевозок нашей компании - это доставка грузов по маршруту из Москвы в Санкт-Петербург, а также из Санкт-Петербурга в Москву.</p>
+          <p>Благодаря наличию собственного автотранспорта и четко выстроенной схеме движения, мы доставим Ваш груз между столицами на следующий день до нашего склада или адреса получателя. Все машины оборудованы инновационными системами навигации, что дает возможность отслеживать груз во время всей перевозки.</p>
+          <p>Доставка грузов по данному маршруту осуществляется ежедневно. Мы выполняем доставку «Москва ⇄ Санкт-Петербург» как в составе сборного груза, так и индивидуальным автотранспортом.</p>
+        </div>
+      </div>
+    </section>
+    <section>
+      <form action="" class="main__form">
+        <div class="form__left">
+          <input type="text" placeholder="Плательщик*" required>
+          <input type="email" placeholder="Email*" required>
+          <input type="phone" placeholder="Телефон*" required>
+          <input type="text" placeholder="Контактное лицо*" required>
+          <input type="text" placeholder="Серия и номер паспорта*" required>
+          <input type="text" placeholder="Наименование груза">
+          <div class="form__left__line">
+            <input type="text" placeholder="Мест" required>
+            <input type="text" placeholder="Вес, кг." required>
+            <input type="text" placeholder="Объем, м3" required>
+          </div>
+          <input type="text" placeholder="Объявленная стоимость (страхование)*" required>
+        </div>
+        <div class="form__right">
+          <div class="form__right__ling">
+            <input type="text" placeholder="Отправитель*" required>
+            <input type="text" placeholder="Получатель*" required>
+          </div>
+          <div class="form__right__ling">
+            <input type="text" placeholder="Пункт отправления*" required>
+            <input type="text" placeholder="Пункт назначения*" required>
+          </div>
+          <div class="form__right__ling">
+            <input type="text" placeholder="Адрес подачи автомобиля">
+            <input type="text" placeholder="Адрес доставки">
+          </div>
+          <div class="form__right__ling">
+            <input type="text" placeholder="Телефон*" required>
+            <input type="text" placeholder="Телефон*" required>
+          </div>
+          <div class="form__right__ling">
+            <input type="text" placeholder="Контактное лицо*" required>
+            <input type="text" placeholder="Контактное лицо*" required>
+          </div>
+          <textarea name="" id="" cols="30" rows="10" placeholder="Дополнительная информация"></textarea>
+          <input type="text" placeholder="Город оплаты" required>
+          <button>Отправить заявку</button>
+        </div>
+      </form>
+    </section>
+    <section class="contact">
+    <a href="https://yandex.ru/maps/?um=constructor%3Acee90f2a0adf2d258a8dc0bd9db71106b2b1d991717c7db439d9bced02c687ca&amp;source=constructorStatic" target="_blank"><img src="https://api-maps.yandex.ru/services/constructor/1.0/static/?um=constructor%3Acee90f2a0adf2d258a8dc0bd9db71106b2b1d991717c7db439d9bced02c687ca&amp;width=570&amp;height=320&amp;lang=ru_RU" alt="" style="border: 0;" /></a>
+    <div>
+      <div class="contact__content">
+        <h6>Контакты</h6>
+        <p>Ростов-На-Дону</p>
+        <p>ул. Иловайская, дом 3, строение 18,
+            Складской комплекс «Деметра».
+            </p>
+        <a href="#">+7 (495) 260-68-53</a>
+      </div>
+    </div>
+    </section>
+    <footer>
+      <img src="img/logo.svg" alt="Logo">
+      <p>Сайт является объектом авторского права. Без согласия правообладателя копирование запрещено.</p>
+    </footer>
+  </div>
+  <script src="js/tabs.js"></script>
+</body>
+
+</html>
